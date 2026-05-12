83e9d90e42a9f35731c987d4f06bf46a30444cb5
Matthew O'Connor <oconnormattc@gmail.com>
2026-05-12 14:55:38 -0400

    Modified live count update as well as update function. Update may be obsolete

--- db.py
+++ db.py
@@ -44,61 +44,37 @@ def fetch(venue: str, column: str) -> int | str:
 
     return data[0][column]
 
-def update(venue: str, column: str) -> bool:
+# This may be an obsolete function for prod and only good for testing because we can just update the live count with the state sent from
+# the app
+def update(venue: str, column: str, value: int) -> bool:
     """
     Updates either the entered or exited data.
 
     ::param venue the venue string
     ::param column the string to specify people_entered or people_exited
+    ::param value the int value to replace the current value with
     ::return either True if the execution was successful or false if it failed
     """
 
-    col_val = fetch(venue, column)
-
-    if type(col_val) is str:
-        print(col_val)
-        return False
-
-    col_val += 1
-
     try:
-        supabase.table("testIncrement").update({column:col_val}).eq("venue", venue).execute()
+        supabase.table("testIncrement").update({column:value}).eq("venue", venue).execute()
     except Exception as e:
         print(f'Error Updating Data: {e}')
         return False
     
     return True
 
-def update_live_count(venue: str) -> bool:
+def update_live_count(venue: str, value: int) -> bool:
     """
     Updates the live count at the venue by fetching the people entered and exited and doing a subtraction update
 
     ::param venue the venue name
-    ::return True upon success and False otherwise
+    ::param value the live count value sent from the app
+    ::return True upon success and Fvaluealse otherwise
     """
 
-    # Note: this may get expensive for scaling if we continue doing a double query every time we update
-    # Instead we may want to group it into one single query, not sure how much more efficient it will be
-    # But probably better in the long run
-    entered_val = fetch(venue, "people_entered")
-    exited_val = fetch(venue, "people_exited")
-
-    if type(entered_val) is str:
-        print(entered_val)
-        return False
-    
-    if type(exited_val) is str:
-        print(exited_val)
-        return False
-    
-    # May want future error handling for negatives, for now we will do 0
-    live_count = entered_val - exited_val
-
-    if live_count < 0:
-        live_count = 0
-
     try:
-        supabase.table("testIncrement").update({"live_count": live_count}).eq("venue", venue).execute()
+        supabase.table("testIncrement").update({"live_count": value}).eq("venue", venue).execute()
     except Exception as e:
         print(f'Error Updating Live Count: {e}')
         return False
